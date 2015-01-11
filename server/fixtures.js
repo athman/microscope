ca1eed7c4353a7c538e87c68f6c02796c3ff7a37
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2015-01-11 16:25:49 +0300

    >>Added a posts edit form

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,21 +1,25 @@
 if (Posts.find().count() == 0) {
 	Posts.insert({
 		title: 'Introducing Telescope',
-		url: 'http://sachagreif.com/introducing-telescope/'
+		url: 'http://sachagreif.com/introducing-telescope/',
+		author: 'System'
 	});
 
 	Posts.insert({
 		title: 'Meteor',
-		url: 'http://meteor.com'
+		url: 'http://meteor.com',
+		author: 'System'
 	});
 
 	Posts.insert({
 		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
+		url: 'http://themeteorbook.com',
+		author: 'System'
 	});
 
 	Posts.insert({
 		title: 'Github',
-		url: 'https://github.com'
+		url: 'https://github.com',
+		author: 'System'
 	});
 }
